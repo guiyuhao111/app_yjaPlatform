a62aca4d01b88161b34946bed36325b780fd699d
guiyuhao111 <1060455912@qq.com>
2017-12-14 10:44:11 +0800

    增加注释

--- src/main/java/cn/net/gxht/app/yjdPlatform/pictures/controller/PicturesController.java
+++ src/main/java/cn/net/gxht/app/yjdPlatform/pictures/controller/PicturesController.java
@@ -1,23 +1,15 @@
 package cn.net.gxht.app.yjdPlatform.pictures.controller;
 
 import cn.net.gxht.app.yjdPlatform.common.entity.jsonResult.JsonResult;
-import cn.net.gxht.app.yjdPlatform.pictures.entity.MatrixToImageWriter;
 import cn.net.gxht.app.yjdPlatform.pictures.service.PicturesService;
-import com.google.zxing.BarcodeFormat;
-import com.google.zxing.EncodeHintType;
-import com.google.zxing.MultiFormatWriter;
-import com.google.zxing.common.BitMatrix;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import java.io.File;
 import java.io.UnsupportedEncodingException;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -28,19 +20,6 @@ import java.util.Map;
 public class PicturesController {
     @Resource
     private PicturesService picturesService;
-
-    /**
-     * 被废弃的接口不做解释,可以删除
-     *
-     * @param id
-     * @return
-     */
-//    @RequestMapping("/findSonPicture")
-//    @ResponseBody
-//    public JsonResult doFindSonPicture(int id) {
-//        String path = picturesService.findPictureByBelongId(id);
-//        return new JsonResult(path);
-//    }
 
     /**
      * 寻找轮播图图片url
@@ -68,30 +47,7 @@ public class PicturesController {
     }
 
     /**
-     * 被抛弃的接口不做解释
-     *
-     * @return
-     */
-//    @RequestMapping("/findShupai")
-//    @ResponseBody
-//    public JsonResult doFindShupaiPictures() {
-//        Map<String, Object> map = picturesService.findShupaiPictures();
-//        return new JsonResult(map);
-//    }
-
-//    /**
-//     * 被废弃的接口不做解释
-//     *
-//     * @return
-//     */
-//    @RequestMapping("/findRegistPicture")
-//    @ResponseBody
-//    public JsonResult doFindRegistPicture() {
-//        return new JsonResult(picturesService.findRegistPicture());
-//    }
-
-    /**
-     * 寻找所有商家
+     * 商家模糊查询的入口,主要是商家列表页面上方的模糊查询(此接口知道即可)
      *
      * @param request
      * @return
@@ -112,33 +68,6 @@ public class PicturesController {
     }
 
     /**
-     * 写的demo可以删除
-     *
-     * @param request
-     * @param response
-     * @return
-     */
-    @RequestMapping("/getQRCode")
-    @ResponseBody
-    public String getQRCode(HttpServletRequest request, HttpServletResponse response) {
-        String url = request.getSession().getServletContext().getRealPath("/");
-        System.out.println("url:" + url);
-        try {
-            String content = "www.gxht.net.cn";
-            String path = url;
-            MultiFormatWriter multiFormatWriter = new MultiFormatWriter();
-            Map hints = new HashMap();
-            hints.put(EncodeHintType.CHARACTER_SET, "UTF-8");
-            BitMatrix bitMatrix = multiFormatWriter.encode(content, BarcodeFormat.QR_CODE, 400, 400, hints);
-            File file1 = new File(path, "gxhtLTD.jpg");
-            MatrixToImageWriter.writeToFile(bitMatrix, "jpg", file1);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return url;
-    }
-
-    /**
      * 返回商家入驻时所有选择的入驻类型
      *
      * @return
@@ -149,8 +78,6 @@ public class PicturesController {
         List<Map<String, Object>> list = picturesService.findSelectOption();
         return new JsonResult(list);
     }
-
-    ;
 
     /**
      * 根据图片id寻找图片(可以和寻找所有图片的接口写在一起)
